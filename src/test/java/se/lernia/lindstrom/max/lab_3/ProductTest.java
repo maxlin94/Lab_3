3ffe6d70d3b4aa6fe8b6ca98290104ba680c89a2
Max Lindström <max.lindstrom2@lerniastudent.se>
2024-09-14 16:59:55 +0200

    Added test for valid product and refactor

--- src/test/java/se/lernia/lindstrom/max/lab_3/ProductTest.java
+++ src/test/java/se/lernia/lindstrom/max/lab_3/ProductTest.java
@@ -6,41 +6,57 @@ import se.lernia.lindstrom.max.lab_3.entities.Product;
 
 import java.time.LocalDateTime;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ProductTest {
+    private static final LocalDateTime now = LocalDateTime.now();
 
     @Test
-    void testEmptyOrNullName() {
-        assertThrows(IllegalArgumentException.class, () ->
-                new Product("5", "", Category.JEANS, 7, LocalDateTime.now(), LocalDateTime.now()));
-        assertThrows(IllegalArgumentException.class, () ->
-                new Product("5", null, Category.JEANS, 7, LocalDateTime.now(), LocalDateTime.now()));
+    void testValidProduct() {
+        Product product = new Product("1", "Shirt", Category.SHIRT, 8, now, now);
+        assertNotNull(product);
     }
 
     @Test
-    void testEmptyOrNullId() {
+    void testNotEmptyOrNullName() {
         assertThrows(IllegalArgumentException.class, () ->
-            new Product("", "Shirt", Category.SHIRT, 7, LocalDateTime.now(), LocalDateTime.now()));
+                new Product("5", "", Category.JEANS, 7, now, now));
         assertThrows(IllegalArgumentException.class, () ->
-            new Product(null, "Shirt", Category.SHIRT, 7, LocalDateTime.now(), LocalDateTime.now()));
+                new Product("5", null, Category.JEANS, 7, now, now));
     }
 
     @Test
-    void testNullCategory() {
+    void testNotEmptyOrNullId() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new Product("", "Shirt", Category.SHIRT, 7, now, now));
+        assertThrows(IllegalArgumentException.class, () ->
+                new Product(null, "Shirt", Category.SHIRT, 7, now, now));
+    }
+
+    @Test
+    void testNotRatingTooHighOrLow() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new Product("1", "Shirt", Category.SHIRT, 17, now, now));
+        assertThrows(IllegalArgumentException.class, () ->
+                new Product(null, "Shirt", Category.SHIRT, -1, now, now));
+    }
+
+    @Test
+    void testNotNullCategory() {
         assertThrows(NullPointerException.class, () ->
-            new Product("5", "Shirt", null, 7, LocalDateTime.now(), LocalDateTime.now()));
+                new Product("5", "Shirt", null, 7, now, now));
     }
 
     @Test
-    void testNullCreationDate() {
+    void testNotNullCreationDate() {
         assertThrows(NullPointerException.class, () ->
-            new Product("5", "Shirt", Category.SHIRT, 7, null, LocalDateTime.now()));
+                new Product("5", "Shirt", Category.SHIRT, 7, null, now));
     }
 
     @Test
-    void testNullModificationDate() {
+    void testNotNullModificationDate() {
         assertThrows(NullPointerException.class, () ->
-            new Product("5", "Shirt", Category.SHIRT, 7, LocalDateTime.now(), null));
+                new Product("5", "Shirt", Category.SHIRT, 7, now, null));
     }
 }
