5c31c975341aa88961a0404d279007dc8c8ff6a7
Max Lindström <max.lindstrom2@lerniastudent.se>
2024-09-14 16:34:46 +0200

    Add more validation for creating products

--- src/main/java/se/lernia/lindstrom/max/lab_3/entities/Product.java
+++ src/main/java/se/lernia/lindstrom/max/lab_3/entities/Product.java
@@ -1,6 +1,7 @@
 package se.lernia.lindstrom.max.lab_3.entities;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public record Product(
         String id,
@@ -14,5 +15,14 @@ public record Product(
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("Product name cannot be null or blank");
         }
+        if (id == null || id.isBlank()) {
+            throw new IllegalArgumentException("Product id cannot be null or blank");
+        }
+        if (rating < 0 || rating > 10) {
+            throw new IllegalArgumentException("Product rating must be between 0 and 10");
+        }
+        Objects.requireNonNull(category, "Category cannot be null");
+        Objects.requireNonNull(creationDate, "Creation date cannot be null");
+        Objects.requireNonNull(lastModifiedDate, "Last modification date cannot be null");
     }
 }
